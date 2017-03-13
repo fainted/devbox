b9e0e1b87eb54057b84507b9543847551093fcb5
陈昊天 <chenhaotian93@gmail.com>
2017-03-13 12:21:48 +0800

    fix some error in python/pip setup

--- scripts/dev/python.sh
+++ scripts/dev/python.sh
@@ -1,14 +1,31 @@
 #!/bin/bash
 
 printf "Installing pip..."
-
-# PyPI使用豆瓣源
-# 安装virtualenv, shadowsocks, mycli
 sudo apt-get -y install python-pip python-dev
-mkdir -p ~/.config/pip
-# PyPI使用豆瓣源
-echo '[global]'                                    > ~/.config/pip/pip.conf
-echo 'index-url = https://pypi.douban.com/simple' >> ~/.config/pip/pip.conf
-echo 'trusted-host = pypi.douban.com'             >> ~/.config/pip/pip.conf
 
-sudo pip install virtualenv shadowsocks mycli
+sudo pip install -i https://pypi.douban.com/simple --upgrade pip
+
+# pip setting
+PIP_CONFIG_DIR="/home/vagrant/.config/pip"
+mkdir -p $PIP_CONFIG_DIR
+rm --force $PIP_CONFIG_DIR/pip.conf
+tee -a $PIP_CONFIG_DIR/pip.conf <<EOF
+[global]
+# 使用豆瓣源加速
+index-url = https://pypi.douban.com/simple
+trusted-host = pypi.douban.com
+EOF
+
+# 同步pip配置
+sudo mkdir -p /root/.config/pip
+sudo rm --force /root/.config/pip/pip.conf
+sudo cp $PIP_CONFIG_DIR/pip.conf /root/.config/pip/pip.conf
+
+# 安装模块
+# virtualenv
+# shadowsocks
+# mycli
+# icdiff
+sudo pip install virtualenv shadowsocks mycli icdiff
+
+# TODO setup pip auto completion
